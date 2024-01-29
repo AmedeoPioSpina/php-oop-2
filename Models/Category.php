17b03277c678d584df5525a063bb0d304609a5cb
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-01-29 09:16:30 +0100

    problemi sull'impostazione della classe

--- Models/Category.php
+++ Models/Category.php
@@ -1,16 +1,16 @@
 <?php 
     class Category {
         public $name;
-        // public $specie;
-        // public $articleTypology;
-        // public $iconCategoryUrl;
+        public $specie;
+        public $articleTypology;
+        public $iconCategoryUrl;
 
-        public function __construct(string $_name)
+        public function __construct(string $_name, string $_specie, string $_articleTypology, string $_iconCategoryUrl)
         {
             $this->name = $_name;
-            // $this->specie = $_specie;
-            // $this->articleTypology = $_articleTypology;
-            // $this->iconCategoryUrl = $_iconCategoryUrl;
+            $this->specie = $_specie;
+            $this->articleTypology = $_articleTypology;
+            $this->iconCategoryUrl = $_iconCategoryUrl;
         }
     }
 ?>
